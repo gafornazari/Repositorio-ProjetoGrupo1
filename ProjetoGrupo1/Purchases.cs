bb12ca5ebaadf7c6cdb7f0b7e7c9edfdd20bf5e7
FelipeLMoreir <felipe.lm.moreira@gmail.com>
2025-10-23 15:58:46 -0300

    Branch Felipe

---
+++ ProjetoGrupo1/Purchases.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoGrupo1
+{
+    internal class Purchases
+    {
+        public int Id { get; private set; }
+        public DateOnly DataCompra { get; private set; } 
+        public string Fornecedor { get; private set; }
+        public double ValorTotal { get; private set; }
+        public Purchases(int id, DateOnly dataCompra, 
+            /*string fornecedor*/ double valorTotal)
+        {
+            Id = id;
+            DataCompra = dataCompra;
+            //Fornecedor = fornecedor;
+            ValorTotal = valorTotal;
+        }
+        public string DataFormatada()
+        {
+            return DataCompra.ToString("dd/MM/yyyy");
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Data da Compra: {DataFormatada()}, " +
+                //$"Fornecedor: {Fornecedor}, " +
+                $"Valor Total: {ValorTotal}";
+        }
+        public string ToFile()
+        {
+            return $"{Id}{DataFormatada()}" +//$"{Fornecedor}" +
+                $"{ValorTotal}";
+        }
+    }
+}
